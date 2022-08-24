6dced9c7b9a6249533d4f7bc51c3b4029f64ff84
tyomhk <cheertrain15@naver.com>
2022-08-25 00:08:33 +0900

    useQuery w/ variables for conveying arguments.

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -2,6 +2,8 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/',
+
+  // Data, that have been fetched before, won't be fetched again.
   cache: new InMemoryCache(),
 });
 
--- src/routes/Musics.js
+++ src/routes/Musics.js
@@ -1,13 +1,13 @@
 import style from './Musics.module.scss';
 import { gql, useQuery } from "@apollo/client";
+import { Link } from 'react-router-dom';
 
 const ALL_MUSICS = gql`
   query getAllMusics {
     allMusics {
       id
       title
-      reading
-      image
+      artist
     }
   }
 `;
@@ -16,22 +16,15 @@ function Musics() {
   // https://new.chunithm-net.com/chuni-mobile/html/mobile/img/
   const { data } = useQuery(ALL_MUSICS);
 
-  console.log(data);
-
   return (
     <div>
       List of Musics
       {data && data.allMusics.map((music, index) => (
-        <li className={style['music-list']} key={`${music.title}${index}`}>
-          <p>{music.title}</p>
-          <p>{music.reading}</p>
-          <p>{music.artist}</p>
-          <div>
-            <img
-              src={`https://new.chunithm-net.com/chuni-mobile/html/mobile/img/${music.image}`}
-              alt={music.title}
-            />
-          </div>
+        <li className={style['music-list']} key={`${music.id}`}>
+          <Link to={`/music/${music.id}`}>
+            <p>{music.title}</p>
+            <p>{music.artist}</p>
+          </Link>
         </li>
       ))}
     </div>
